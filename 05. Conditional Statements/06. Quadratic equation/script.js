bbb0e9d89412e88d1a09f3a28eab409fa6315e5a
AyrFX <oleg@gbg.bg>
2016-06-16 22:53:40 +0300

    Corrected task 6 of Conditional Statements Homework

--- 05. Conditional Statements/06. Quadratic equation/script.js
+++ 05. Conditional Statements/06. Quadratic equation/script.js
@@ -2,21 +2,19 @@ function solve(args) {
     var aNumber = +args[0],
         bNumber = +args[1],
         cNumber = +args[2],
-        dValue = bNumber * bNumber - 4 * aNumber * cNumber;
+        dValue = bNumber * bNumber - (4 * aNumber * cNumber);
     if (dValue < 0) {
         console.log('no real roots');
     } else if (dValue == 0) {
         var root = (bNumber * -1) / (2 * aNumber);
         console.log('x1=x2=' + root.toFixed(2));
     } else {
-        var root1 = (bNumber * -1 + Math.sqrt(dValue)) / 2 * aNumber,
-            root2 = (bNumber * -1 - Math.sqrt(dValue)) / 2 * aNumber;
-        console.log('x1=' + root1.toFixed(2) + ';x2=' + root2.toFixed(2));
+        var root1 = ((bNumber * -1) + Math.sqrt(dValue)) / (2 * aNumber),
+            root2 = ((bNumber * -1) - Math.sqrt(dValue)) / (2 * aNumber);
+        if (root1 < root2) {
+            console.log('x1=' + root1.toFixed(2) + '; x2=' + root2.toFixed(2));
+        } else {
+            console.log('x1=' + root2.toFixed(2) + '; x2=' + root1.toFixed(2));
+        }
     }
 }
-
-solve([2, 5, -3]);
-solve([-1, 3, 0]);
-solve([-0.5, 4, -8]);
-solve([5, 2, 8]);
-solve([0.2, 9.572, 0.2]);
