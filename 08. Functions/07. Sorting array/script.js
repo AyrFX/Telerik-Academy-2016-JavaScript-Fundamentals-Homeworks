42860888896eb30c41fbf878c66d4c6b646b211a
AyrFX <oleg@gbg.bg>
2016-06-28 07:23:13 +0300

    Added task 7 of Functions homework gully working

--- 08. Functions/07. Sorting array/script.js
+++ 08. Functions/07. Sorting array/script.js
@@ -1,20 +1,37 @@
 function solve(args) {
     function GetMaximalElement(array, startIndex) {
-        var maximalElement = array[startIndex];
+        var maximalElement = {};
+        maximalElement.value = array[startIndex];
+        maximalElement.index = startIndex;
         for (var i = startIndex + 1; i < array.length; i++) {
-            if (maximalElement < array[i]) {
-                maximalElement = array[i];
+            if (maximalElement.value < array[i]) {
+                maximalElement.value = array[i];
+                maximalElement.index = i;
             }
         }
         return maximalElement;
     }
 
-    function SortArray(array) {
-        var sordetArray;
-        for (var i = array.length - 1; i >= 0; i += 1) {
-
+    function SortArrayDescending(array) {
+        var maxElement, temp;
+        for (var i = 0; i < array.length; i += 1) {
+            maxElement = GetMaximalElement(array, i);
+            temp = maxElement.value;
+            array[maxElement.index] = array[i];
+            array[i] = temp;
         }
+        return array;
     }
+
+    function SortArrayAscending(array) {
+        return SortArrayDescending(array).reverse();
+    }
+
+    var arrayLength = +args[0],
+        array = args[1].split(' ').map(Number),
+        sortedArray = SortArrayAscending(array);
+
+    console.log(array.join(' '));
 }
 
-solve(['6\n-26 -25 -28 31 2 27']);
+solve(['6', '-26 -25 -28 31 2 27']);
