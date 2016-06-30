99eeec9ecf746910870fc23650d49d93b1427e83
AyrFX <oleg@gbg.bg>
2016-06-30 22:45:40 +0300

    Strings HW, task 8: added empty file with test input from BGCoder

---
+++ 11. Strings/08. Replace tags/script.js
@@ -0,0 +1,7 @@
+function solve(args) {
+
+}
+
+solve([
+    '<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>'
+]);
