b7337f0e513e5a7ae1300f493afef934ecd4b585
AyrFX <oleg@gbg.bg>
2016-06-30 17:09:26 +0300

    Strings HW, task 4: added (third version is optimized)

---
+++ 11. Strings/04. Parse tags/script.js
@@ -0,0 +1,50 @@
+function solve(args) {
+    let text = args[0],
+        state = [],
+        readTag,
+        currentTag = '',
+        tags = [],
+        result = [];
+
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] === '<') {
+            state.push('inTag');
+            readTag = true;
+        } else if (text[i] === '>' && state[state.length - 1] === 'inTag') {
+            state.pop();
+            readTag = false;
+            currentTag += text[i];
+            tags.push(currentTag);
+            currentTag = '';
+        } else if (state[state.length - 1] !== 'inTag') {
+            if (tags.length === 0) {
+                result.push(text[i]);
+            } else if (tags[tags.length - 1].indexOf('upcase') !== -1) {
+                result.push(text[i].toUpperCase());
+            } else if (tags[tags.length - 1].indexOf('lowcase') !== -1) {
+                result.push(text[i].toLowerCase());
+            } else {
+                result.push(text[i]);
+            }
+        }
+
+        if (readTag || text[i] === '>') {
+            currentTag += text[i];
+        }
+
+        if (tags.length !== 0 && tags[tags.length - 1].indexOf('</') !== -1) {
+            tags.pop();
+            tags.pop();
+        }
+    }
+
+    console.log(result.join(''));
+    //console.log(tags);
+}
+
+solve([
+    'We are <orgcase>liViNg</orgcase> in a <upcase>yellow submarine</upcase>. We <orgcase>doN\'t</orgcase> have <lowcase>anything</lowcase> else.'
+]);
+solve([
+    'We are <orgcase>liViNg</orgcase> in a <upcase>yellow <orgcase>sub<upcase>mar</upcase>ine</orgcase></upcase>. We <orgcase>doN\'t</orgcase> have <lowcase>anything</lowcase> else.'
+]);
