b9e73edcb42fc59ce861b68b65ebfd75fdf89d8a
AyrFX <oleg@gbg.bg>
2016-06-30 15:32:47 +0300

    Strings HW, task 1: added

---
+++ 11. Strings/01. Reverse string/script.js
@@ -0,0 +1,14 @@
+function solve(args) {
+    let text = args[0],
+        reversedAsArray = [];
+
+    for (var i = text.length - 1; i >= 0; i -= 1) {
+        reversedAsArray.push(text[i]);
+    }
+
+    console.log(reversedAsArray.join(''));
+}
+
+solve(['sample']);
+
+solve(['qwertytrewq']);
