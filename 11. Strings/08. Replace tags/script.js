73f33aa6fb86b18209bd70811237ca6de9d23922
AyrFX <oleg@gbg.bg>
2016-07-01 11:13:34 +0300

    Strings HW, task 8: added, but hit the time limit for all tests

--- 11. Strings/08. Replace tags/script.js
+++ 11. Strings/08. Replace tags/script.js
@@ -1,5 +1,43 @@
 function solve(args) {
+    let text = args[0],
+        currentTagPositions,
+        link,
+        description;
 
+    function getFirstAnchor(text) {
+        let openTagStart = text.indexOf('<a'),
+            openTagEnd,
+            closeTagStart,
+            closeTagEnd;
+
+        if (openTagStart === -1) {
+            openTagEnd = -1;
+            closeTagStart = -1;
+            closeTagEnd = -1;
+        } else {
+            openTagEnd = text.indexOf('>', openTagStart + 1);
+            closeTagStart = text.indexOf('</a>', openTagEnd + 1);
+            closeTagEnd = text.indexOf('>', closeTagStart);
+        }
+
+        return {
+            openTagStart: openTagStart,
+            openTagEnd: openTagEnd,
+            closeTagStart: closeTagStart,
+            closeTagEnd: closeTagEnd
+        };
+    }
+
+    currentTagPositions = getFirstAnchor(text);
+    while (currentTagPositions.openTagStart !== -1) {
+        link = text.slice(currentTagPositions.openTagStart + 9, currentTagPositions.openTagEnd - 1);
+        description = text.slice(currentTagPositions.openTagEnd + 1, currentTagPositions.closeTagStart);
+        text = text.replace(text.substring(currentTagPositions.openTagStart, currentTagPositions.closeTagEnd + 1),
+            '[' + description + '](' + link + ')');
+        currentTagPositions = getFirstAnchor(text);
+    }
+
+    console.log(text);
 }
 
 solve([
